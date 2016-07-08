f93879f95a293c7438b32e5ebc7e597e0d89db85
Your Name <you@example.com>
2016-07-08 17:33:03 +0800

    first commit

---
+++ MiniWeather/app/src/main/java/pack/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package pack.miniweather;
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by zzp on 2016/7/8.
+ */
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
+
