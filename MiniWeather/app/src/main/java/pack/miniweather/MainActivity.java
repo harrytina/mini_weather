f87ccdfffd39c74a9763b45b56776f4a91e0bae7
harrytina <nwnulijing@nwnu.edu.cn>
2016-07-09 15:49:49 +0800

    v79-2

--- MiniWeather/app/src/main/java/pack/miniweather/MainActivity.java
+++ MiniWeather/app/src/main/java/pack/miniweather/MainActivity.java
@@ -1,6 +1,10 @@
 package pack.miniweather;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
+import pack.util.NetUtil;
 
 /**
  * Created by zzp on 2016/7/8.
@@ -9,6 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
+        }
     }
 }
 
